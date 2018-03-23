72f0a47a2583d82f0ed088947d48c15cc1e7547e
W3ndige <w3ndige@gmail.com>
2018-03-23 20:56:36 +0100

    Failure in implementing the send answers, still have to rethink that

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -8,6 +9,9 @@
 #include <sys/socket.h>
 
 // TODO(W3ndige): Error handling.
+// TODO(W3ndige): Sending end of connection message.
+// TODO(W3ndige): Maximum buffer protection
+// TODO(W3ndige): Client have to confirm that he got the question, only then the server will send another message.
 
 const int PORT = 1337;
 const int MAX_RECEIVE_BUFFER = 500;
@@ -15,26 +19,33 @@ const int MAX_RECEIVE_BUFFER = 500;
 int main(int argc, char *argv[]) {
    char buffer[MAX_RECEIVE_BUFFER];
    struct sockaddr_in destination;
-
    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
    memset(&destination, 0, sizeof(struct sockaddr_in));
+
    destination.sin_family = AF_INET;
-   destination.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+   destination.sin_addr.s_addr = inet_addr("192.168.0.248");
    destination.sin_port = htons(PORT);
 
-   connect(socket_fd, (struct sockaddr *)&destination, sizeof(struct sockaddr_in));
+   if (connect(socket_fd, (struct sockaddr *)&destination, sizeof(struct sockaddr_in)) == -1) {
+     perror("Error while connecting to server");
+     return 1;
+   }
 
    while (1) {
      int len = recv(socket_fd, buffer, sizeof(buffer),0);
      buffer[len] = '\0';
-     printf("New message: %s\n", buffer);
+     printf("New question: %s\n", buffer);
+     for (int i = 0; i < 4; i++) {
+       memset(buffer, 0, sizeof(buffer) / sizeof(buffer[0]));
+       len = recv(socket_fd, buffer, sizeof(buffer),0);
+       buffer[len] = '\0';
+       printf("%i. %s\n", i + 1, buffer);
+     }
      printf("Enter your response:\n");
      fgets(buffer, MAX_RECEIVE_BUFFER, stdin);
      send(socket_fd, buffer, strlen(buffer), 0);
    }
 
-
    close(socket_fd);
-   return EXIT_SUCCESS;
+   return 1;
 }
--- server.c
+++ server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -7,15 +8,43 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-// TODO(W3ndige): Add error handling.
+// TODO(W3ndige): Client verification.
+// TODO(W3ndige): Change buffer into dynamic data type.
 
 const int PORT = 1337;
 const int MAX_RECEIVE_BUFFER = 500;
 
-int main(int argc, char *argv[]) {
+void handleClient(int socket_fd, socklen_t socket_size, struct sockaddr_in destination, char *questions, char **answers, int correct_answer) {
   char buffer[MAX_RECEIVE_BUFFER];
-  char *message = "Hello there my slave.";
+  int client_fd = accept(socket_fd, (struct sockaddr *)&destination, &socket_size);
+  printf("New connection from: %s\n", inet_ntoa(destination.sin_addr));
+  while (1) {
+    send(client_fd, questions, strlen(questions), 0);
+    printf("Sending questions: %s\n", questions);
 
+    for (size_t i = 0; i < sizeof(questions) / 2; i++) {
+      send(client_fd, answers[i], strlen(answers[i]), 0);
+      printf("%ld. %s\n", i + 1, answers[i]);
+    }
+
+    int len = recv(client_fd, buffer, MAX_RECEIVE_BUFFER,0);
+    if (len == -1) {
+      perror("Error receiving data");
+      return;
+    }
+    if (len == 1 || len == 0) {
+      printf("Dropping connection from: %s\n", inet_ntoa(destination.sin_addr));
+      return;
+    }
+    else {
+      buffer[len] = '\0';
+      printf("Got: %s\n", buffer);
+    }
+  }
+  close(client_fd);
+}
+
+int main(int argc, char *argv[]) {
   struct sockaddr_in destination;
   struct sockaddr_in server;
 
@@ -29,31 +58,21 @@ int main(int argc, char *argv[]) {
   server.sin_port = htons(PORT);
 
   int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_fd == -1) {
+    perror("Couldn't create socket");
+    return 1;
+  }
   bind(socket_fd, (struct sockaddr *)&server, sizeof(struct sockaddr));
-
   listen(socket_fd, 1);
 
+  char *questions = "Where would you find the Sea of Tranquility?";
+  char *answers[4] = {"Mars", "Titan", "Venus", "Moon"};
+  int correct_answer = 3;
+
   while (1) {
-    int client_fd = accept(socket_fd, (struct sockaddr *)&destination, &socket_size);
-    printf("New connection from: %s\n", inet_ntoa(destination.sin_addr));
-
-    while (1) {
-      send(client_fd, message, strlen(message), 0);
-      printf("Sending: %s\n", message);
-
-      int len = recv(client_fd, buffer, MAX_RECEIVE_BUFFER,0);
-
-      if (len == 1) {
-        printf("Dropping connection from: %s\n", inet_ntoa(destination.sin_addr));
-        break;
-      }
-
-      buffer[len] = '\0';
-      printf("Got: %s\n", buffer);
-    }
-    close(client_fd);
+    handleClient(socket_fd, socket_size, destination, questions, answers, correct_answer);
   }
 
   close(socket_fd);
-  return EXIT_SUCCESS;
+  return 1;
 }
